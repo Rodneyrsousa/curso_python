8f3db6167501d34b27e5b4c011d8902815fc06bf
Rodney Rodrigues <rodney.rsousa@gmail.com>
2023-03-17 11:34:49 -0300

    primeiro commit do diretório

---
+++ aula119.py
@@ -0,0 +1,12 @@
+pessoa = {
+    'nome': 'Rodney',
+    'sobrenome': 'Rodrigues',
+    'altura': '1.7',
+    'endereco': [
+        {'rua': 'turquesa', 'numero': 169},
+    
+    ],
+}
+
+print(pessoa)
+print(pessoa['sobrenome'])
