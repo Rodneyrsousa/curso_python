591ae4346d3a06a9380a41a87b604c67361054fd
Rodney Rodrigues <rodney.rsousa@gmail.com>
2023-03-30 11:09:11 -0300

    Atualização aula 119

--- aula119.py
+++ aula119.py
@@ -1,12 +1,35 @@
-pessoa = {
-    'nome': 'Rodney',
-    'sobrenome': 'Rodrigues',
-    'altura': '1.7',
-    'endereco': [
-        {'rua': 'turquesa', 'numero': 169},
+# pessoa = {
+#     'nome': 'Rodney',
+#     'sobrenome': 'Rodrigues',
+#     'altura': '1.7',
+#     'endereco': [
+#         {'rua': 'turquesa', 'numero': 169},
     
-    ],
-}
+#     ],
+# }
 
-print(pessoa)
-print(pessoa['sobrenome'])
+# print(pessoa)
+# print(pessoa['sobrenome'])
+
+
+
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+# lista.sort(key=lambda item: item['nome'])
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+    
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
